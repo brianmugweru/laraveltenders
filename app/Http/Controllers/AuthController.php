d22d5b252696bbb9d253604fc492467c8236b1b6
Brian Muugweru <brianmugweru@gmail.com>
2017-03-27 18:34:35 +0300

    add validation rules for form login and logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,13 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class AuthController extends Controller
 {
-  public function login(){
+  public function login()
+  {
     return view('login');
   }
-  public function handlelogin(request $request){
+
+  public function handlelogin(Request $request)
+  {
+    $this -> validate($request, User::$login_validation_rules);
+    $data = $request->only('email','password');
+    if(\Auth::attempt($data)){
+      return redirect()->intended('dashboard');
+    }
+    return back()->withInput()->withErrors(['email'=>'Username or password is Wrong']);
 
   }
 }
