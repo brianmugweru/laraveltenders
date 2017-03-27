d8a08102cca462c0366ec7261b48680fb03ca190
Brian Muugweru <brianmugweru@gmail.com>
2017-03-27 15:41:32 +0300

    add addition fillable form fiels details to app user variable

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+      'firstname','lastname','pnumber','email','password','company','address','city','province','country'
     ];
 
     /**
