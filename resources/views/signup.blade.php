215c58289d186cb3fa917bcf978825256a3abbd3
Brian Muugweru <brianmugweru@gmail.com>
2017-03-27 15:46:32 +0300

    remove unrequired form fields

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -10,73 +10,68 @@
 <div class="row">
 </div>
 <div class="row">
-  <form class="col s12" action="/auth/signup" method="post" >
-    <h4>Organisation Details</h4>
-    <div class="row">
-      <div class="input-field col s12">
-        <input id="last_name" type="email" name="email" class="validate">
-        <label for="last_name">Legal Company name</label>
+  <div class="col s12">
+    {!! Form::open(array('route' => 'users.store'))  !!}
+      <h4>Organisation Details</h4>
+      <div class="row">
+        <div class="input-field col s12">
+          {!! Form::email('company', null, array('class'=>'validate')) !!}
+          {!! Form::label('Company Name') !!}
+        </div>
       </div>
-    </div>
-    <div class="row">
-      <div class="input-field col s6">
-        <input id="last_name" type="email" name="email" class="validate">
-        <label for="last_name">Address 1</label>
+      <div class="row">
+        <div class="input-field col s6">
+          {!! Form::text('address', null, array('class'=>'validate')) !!}
+          {!! Form::label('Address' ) !!}
+        </div>
+        <div class="input-field col s6">
+          {!! Form::text('city', null, array('class'=>'validate')) !!}
+          {!! Form::label('City' ) !!}
+        </div>
+        <div class="input-field col s6">
+          {!! Form::text('province', null, array('class'=>'validate')) !!}
+          {!! Form::label('province' ) !!}
+        </div>
+        <div class="input-field col s6">
+          {!! Form::text('country', null, array('class'=>'validate')) !!}
+          {!! Form::label('Country' ) !!}
+        </div>
       </div>
-      <div class="input-field col s6">
-        <input id="last_name" type="email" name="email" class="validate">
-        <label for="last_name">Address 2</label>
+      <h4>Contacts</h4>
+      <div class="row">
+        <div class="input-field col s6">
+          <input id="firstname" type="text" name="firstname" class="validate">
+          <label for="firstname">First Name</label>
+        </div>
+        <div class="input-field col s6">
+          <input id="lastname" type="text" name="lastname" class="validate">
+          <label for="lastname">Last Name</label>
+        </div>
+        <div class="input-field col s6">
+          <input id="email" type="email" name="email" class="validate">
+          <label for="email">Email</label>
+        </div>
+        <div class="input-field col s6">
+          <input id="pnumber" type="text" name="pnumber" class="validate">
+          <label for="pnumber">Phone Number</label>
+        </div>
+        <div class="input-field col s6">
+          <input id="password" type="password" name="password" class="validate">
+          <label for="password">Password</label>
+        </div>
+        <div class="input-field col s6">
+          <input id="password2" type="password" name="password2" class="validate">
+          <label for="password2">Confirm Password</label>
+        </div>
       </div>
-      <div class="input-field col s6">
-        <input id="last_name" type="email" name="email" class="validate">
-        <label for="last_name">City</label>
+      <div class="row">
+        <div class="input-field col s6">
+          {!! Form::token() !!}
+          <input class="btn" type="submit" name="submit" value="Register">
+        </div>
       </div>
-      <div class="input-field col s6">
-        <input id="last_name" type="email" name="email" class="validate">
-        <label for="last_name">Postal Code/Zip Code</label>
-      </div>
-      <div class="input-field col s6">
-        <input id="last_name" type="email" name="email" class="validate">
-        <label for="last_name">Province/ State</label>
-      </div>
-      <div class="input-field col s6">
-        <input id="last_name" type="email" name="email" class="validate">
-        <label for="last_name">Country</label>
-      </div>
-    </div>
-    <h4>Contacts</h4>
-    <div class="row">
-      <div class="input-field col s6">
-        <input id="last_name" type="password" name="email" class="validate">
-        <label for="last_name">First Name</label>
-      </div>
-      <div class="input-field col s6">
-        <input id="last_name" type="password" name="email" class="validate">
-        <label for="last_name">Second Name</label>
-      </div>
-      <div class="input-field col s6">
-        <input id="last_name" type="password" name="email" class="validate">
-        <label for="last_name">Email</label>
-      </div>
-      <div class="input-field col s6">
-        <input id="last_name" type="password" name="email" class="validate">
-        <label for="last_name">Phone Number</label>
-      </div>
-      <div class="input-field col s6">
-        <input id="last_name" type="password" name="email" class="validate">
-        <label for="last_name">Password</label>
-      </div>
-      <div class="input-field col s6">
-        <input id="last_name" type="password" name="email" class="validate">
-        <label for="last_name">Confirm Password</label>
-      </div>
-    </div>
-    <div class="row">
-      <div class="input-field col s6">
-        <input class="btn" type="submit" name="submit" value="Register">
-      </div>
-    </div>
-  </form>
+    {!! Form::close() !!}
+  </div>
 </div>
 @include('partials.footer')
 @include('partials.footerscripts')
