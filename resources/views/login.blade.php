3bd8bed47bab8f5522f90cecf07802c7cede6d84
Brian Muugweru <brianmugweru@gmail.com>
2017-03-27 18:37:22 +0300

    add display of errors on login errors

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,20 +8,29 @@ height:58vh;
 <div class="row">
   <div class="col s6 center">
     <h4 class="center teal-text darken-4">Login Form</h4>
+@if(count($errors))
+  <div class="alert success">
+    <ul>
+      @foreach($errors->all() as $error)
+        <li>{{$error }}</li>
+      @endforeach
+    </ul>
+  </div>
+@endif
   </div>
 </div>
   <div class="row">
-    <form class="col s12" action="/auth/login" method="post" >
+{!! Form::open(['url'=>'auth/login','method'=>'post','class'=>'col s12']) !!}
       <div class="row">
         <div class="input-field col s6">
-          <input id="last_name" type="email" name="email" class="validate">
-          <label for="last_name">Email</label>
+          <input id="email" type="email" name="email" class="validate">
+          <label for="email">Email</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s6">
-          <input id="last_name" type="password" name="email" class="validate">
-          <label for="last_name">Password</label>
+          <input id="password" type="password" name="password" class="validate">
+          <label for="password">Password</label>
         </div>
       </div>
       <div class="row">
@@ -29,7 +38,7 @@ height:58vh;
           <input class="btn" type="submit" name="submit" value="login">
         </div>
       </div>
-    </form>
+{!! Form::close() !!}
   </div>
 
 @include('partials.footer')
