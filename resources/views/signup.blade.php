1f8e2582aedfbf31cb8ddc23a5e5b40c097c2532
Brian Muugweru <brianmugweru@gmail.com>
2017-03-27 16:40:02 +0300

    recify user signup form and delete swp file for views

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -15,52 +15,52 @@
       <h4>Organisation Details</h4>
       <div class="row">
         <div class="input-field col s12">
-          {!! Form::email('company', null, array('class'=>'validate')) !!}
+          {!! Form::text('companyname', null, array('class'=>'validate','required')) !!}
           {!! Form::label('Company Name') !!}
         </div>
       </div>
       <div class="row">
         <div class="input-field col s6">
-          {!! Form::text('address', null, array('class'=>'validate')) !!}
+          {!! Form::text('address', null, array('class'=>'validate','required')) !!}
           {!! Form::label('Address' ) !!}
         </div>
         <div class="input-field col s6">
-          {!! Form::text('city', null, array('class'=>'validate')) !!}
+          {!! Form::text('city', null, array('class'=>'validate','required')) !!}
           {!! Form::label('City' ) !!}
         </div>
         <div class="input-field col s6">
-          {!! Form::text('province', null, array('class'=>'validate')) !!}
+          {!! Form::text('province', null, array('class'=>'validate','required')) !!}
           {!! Form::label('province' ) !!}
         </div>
         <div class="input-field col s6">
-          {!! Form::text('country', null, array('class'=>'validate')) !!}
+          {!! Form::text('country', null, array('class'=>'validate','required')) !!}
           {!! Form::label('Country' ) !!}
         </div>
       </div>
       <h4>Contacts</h4>
       <div class="row">
         <div class="input-field col s6">
-          <input id="firstname" type="text" name="firstname" class="validate">
+          <input id="firstname" type="text" name="firstname" class="validate" required>
           <label for="firstname">First Name</label>
         </div>
         <div class="input-field col s6">
-          <input id="lastname" type="text" name="lastname" class="validate">
+          <input id="lastname" type="text" name="lastname" class="validate" required>
           <label for="lastname">Last Name</label>
         </div>
         <div class="input-field col s6">
-          <input id="email" type="email" name="email" class="validate">
+          <input id="email" type="email" name="email" class="validate" required>
           <label for="email">Email</label>
         </div>
         <div class="input-field col s6">
-          <input id="pnumber" type="text" name="pnumber" class="validate">
+          <input id="phonenumber" type="text" name="phonenumber" class="validate" required>
           <label for="pnumber">Phone Number</label>
         </div>
         <div class="input-field col s6">
-          <input id="password" type="password" name="password" class="validate">
+          <input id="password" type="password" name="password" class="validate" required>
           <label for="password">Password</label>
         </div>
         <div class="input-field col s6">
-          <input id="password2" type="password" name="password2" class="validate">
+          <input id="password2" type="password" name="password2" class="validate" required>
           <label for="password2">Confirm Password</label>
         </div>
       </div>
