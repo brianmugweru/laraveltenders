8551c06c945fd774c1d406c30e28c4a19d84052c
Brian Muugweru <brianmugweru@gmail.com>
2017-03-28 22:29:50 +0300

    add relationship contraint to tender db model

--- app/Tender.php
+++ app/Tender.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tender extends Model
 {
-
+  /**
+   * The Users that have bids to tenders
+   */
+    public function users()
+    {
+        return $this->belongsToMany('App\Tender');
+    }
 }
