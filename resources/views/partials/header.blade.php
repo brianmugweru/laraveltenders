c1c872b29ece78a5270974696a679ee9c115e953
Brian Muugweru <brianmugweru@gmail.com>
2017-03-28 22:30:24 +0300

    add users name to header part of page through all views

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,9 +3,10 @@
           <div class="nav-wrapper">
             <a href="/" class="brand-logo">Bids and Tenders</a>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
-              <li><a href="#">Bid Opportunities</a></li>
                 <!--@if (Route::has('login'))
                   @if (Auth::check())-->
+                    <li><a href="#">My Bids</a></li>
+                    <li><a href="#">{{Auth::User()->firstname}} {{Auth::User()->lastname}}</a></li>
                     <li><a href="{{url('/logout') }}">logout</a></li>
                   <!--@else-->
                     <li><a href="{{url('/login') }}">login</a></li>
