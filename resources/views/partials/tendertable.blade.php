2ab9ac47d00ce2bcae26a5495fa22536b257826b
Brian Muugweru <brianmugweru@gmail.com>
2017-05-01 13:04:00 +0300

    add url to download tender file uploads and paginating routes for page

--- resources/views/partials/tendertable.blade.php
+++ resources/views/partials/tendertable.blade.php
@@ -6,6 +6,7 @@
       <th>Description</th>
       <th>Published Date</th>
       <th>Closing Date</th>
+      <th>File Upload</th>
       <th>View/Register</th>
     </tr>
   </thead>
@@ -16,8 +17,11 @@
       <td>{{ $tender->description }}</td>
       <td>{{ $tender->created_at }}</td>
       <td>{{ $tender->closing_on }}</td>
+      <td><a href="{{ route('getfile',$tender->id) }}" download>Tender Upload</a></td>
       <td><a href="{{ URL::to('/tenders/'.$tender->id) }}">View</a></td>
     </tr>
   @endforeach
+  <a href="{{ $tenders->previousPageUrl() }}">prev page</a>
+  <a href="{{ $tenders->nextPageUrl() }}" class="right">next page</a>
  </tbody>
 </table>
