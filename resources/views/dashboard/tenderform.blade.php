45a7a73f636965a5e562355f8580826b948839f0
Brian Muugweru <brianmugweru@gmail.com>
2017-05-01 13:00:33 +0300

    add field for file uploads

--- resources/views/dashboard/tenderform.blade.php
+++ resources/views/dashboard/tenderform.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-  {!! Form::open(['route'=>'tenders.store','method'=>'post','class'=>'col s12']) !!}
+  {!! Form::open(['route'=>'tenders.store','method'=>'post','class'=>'col s12','enctype'=>'multipart/form-data']) !!}
     <div class="row">
       <div class="input-field col s6">
         <input id="bid_name" type="text" name="bid_name" class="validate">
@@ -16,6 +16,19 @@
       <div class="input-field col s6">
         <textarea id="textarea" name="description"class="materialize-textarea"></textarea>
         <label for="textarea">Description</label>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col s6">
+        <div class="file-field input-field">
+          <div class="btn">
+            <span>file</span>
+            <input type="file" name="tender_file" required/>
+          </div>
+          <div class="file-path-wrapper">
+            <input type="text" class="file-path validate" name="" />
+          </div>
+        </div>
       </div>
     </div>
     <div class="row">
