8fbe41ee916e3060855271f601e4094dbaa83efd
Brian Muugweru <brianmugweru@gmail.com>
2017-06-08 19:31:09 +0300

    add mail for account activation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Mail\Mailer;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Mail\Message;
 use Illuminate\Http\Request;
 use App\User;
+use Validator;
 
 class UserController extends Controller
 {
+  protected $mailer;
+    public function __construct(Mailer $mailer)
+    {
+        $this->mailer = $mailer;
+    }
     /**
      * Display a listing of the resource.
      *
@@ -33,12 +42,47 @@ class UserController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+
+    public function activate($email, $code){
+      $actcode = User::where('email',$email)->value('activationCode');
+      if($actcode == $code ){
+        DB::table('users')->where('email',$email)->update(['isActivated'=>1]);
+        return redirect('');
+      }
+    }
     public function store(Request $request)
     {
+      $rules = array(
+          'companyname'=>'required|unique:users,companyname',
+          'email'=>'required|unique:users,email|email',
+          'phonenumber'=>'required|numeric|digits_between:8,12',
+          'password'=>'required|min:5|confirmed',
+      );
+
+      $validator = Validator::make($request->all(),$rules)->validate(); 
+
+      if($validator->fails()){
+          return redirect::to('signup')->with('errors',$validator)->withInput();
+      }
+
       $data = $request->only('firstname','lastname','phonenumber','email','password','companyname','address','city','province','country');
+      $code = str_random(30);
+      $activatecode = array('activationCode'=>$code);
+      $data=array_merge($data,$activatecode);
+      //$data['activationCode'] = str_random(30);
       $data['password'] = bcrypt($data['password']);
+      $link = $data['activationCode'];
+      $message = sprintf("Activation of account with mail <a href='$link'>$link</a>");
       $user = User::create($data);
+
       if($user){
+        \Mail::send(['html'=>'email.activate'], [
+        'data'=>$request->get('email'),
+        'link'=>$link
+          ],function(Message $message) use ($data) {
+            $message ->to($data['email'])->subject("Activation Email");
+          });
+
         \Auth::login($user);
         return redirect('dashboard')->with('status', 'authenticated successfully');
       }
