c0f81ca456acac6aa8bc8307fc77bd79f09e8fbf
Brian Muugweru <brianmugweru@gmail.com>
2017-06-08 19:35:48 +0300

    add view to send to email addresses

---
+++ resources/views/email/activate.blade.php
@@ -0,0 +1,3 @@
+  <h3>Hello, Welcome to tum bids and tenders application</h3>
+  <p>Click on the following link to activate your account</p>
+  <a href="localhost:8000/activate/{{ $data }}/{{ $link }}">localhost:8000/activate/{{ $data }}/{{ $link }}</a></p>
